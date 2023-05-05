5e9384c081ed3c818db3a4f6a4da6ecaa017a52c
Faiz Mohammad Hafidza <faizmohhafidza@gmail.com>
2023-05-05 19:18:20 +0700

    beginning implement tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="bg-blue-300">
       <h1 className="text-3xl text-center text-blue-500 font-extrabold">
         Selamat datang
       </h1>
