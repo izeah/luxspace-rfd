d8e50cac7f77872a0178f5b7f02975f7f539c341
Faiz Mohammad Hafidza <faizmohhafidza@gmail.com>
2023-05-06 09:09:35 +0700

    fill assets and refactor main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,77 @@
-import "./App.css";
+import "./assets/css/App.css";
 
 function App() {
   return (
-    <div className="bg-blue-300">
-      <h1 className="text-3xl text-center text-blue-500 font-extrabold">
-        Selamat datang
-      </h1>
+    <div className="bg-gray-100">
+      <nav className="bg-white shadow-lg">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex-shrink-0">
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                alt="Workflow"
+              />
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <a
+                  href="/"
+                  className="hover:bg-gray-700 text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Home
+                </a>
+                <a
+                  href="/"
+                  className="text-gray-700 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Blog
+                </a>
+                <a
+                  href="/"
+                  className="text-gray-700 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Projects
+                </a>
+                <a
+                  href="/"
+                  className="text-gray-700 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  About
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold leading-tight text-gray-900">
+            Personal Branding
+          </h1>
+          <p className="mt-4 max-w-2xl text-xl text-gray-500">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </header>
+
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <div className="px-4 py-6 sm:px-0">
+            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+          </div>
+        </div>
+      </main>
+
+      <footer className="bg-gray-800">
+        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+          <p className="text-center text-gray-400 text-sm">
+            &copy; 2023 by Personal Branding. All rights reserved.
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
